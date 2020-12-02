7ad741701f527ffdd5cd43ed4ef7f7162e72e308
Nicolas Gabriel <nickcr7cardoso@gmail.com>
2020-12-01 21:53:31 -0300

    Aula 02 finalizada

---
+++ Repositories/AlunoRepository.cs
@@ -0,0 +1,38 @@
+﻿using APIBoletim.Interfaces;
+using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepository : IAluno
+
+    {
+        public Aluno Alterar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno BuscarporId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Cadastrar(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Excluir(Aluno a)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> LerTodos()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
